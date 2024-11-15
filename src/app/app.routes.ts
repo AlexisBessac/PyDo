20742ea0e3875dae2e97be9598604134edae9cbf
PC_ALEXIS\alexis <alexis.bessac@gmail.com>
2024-11-15 17:06:19 +0100

    Même chose que dit précédemment

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,10 +2,12 @@ import { Routes } from '@angular/router';
 import { ConnexionComponent } from './connexion/connexion.component';
 import { InscriptionComponent } from './inscription/inscription.component';
 import { Page404Component } from './page404/page404.component';
+import { AccueilComponent } from './accueil/accueil.component';
 
 export const routes: Routes = [
+    {path: '', component: AccueilComponent }, 
+    {path: "", redirectTo: "accueil", pathMatch: "full" },
     {path: "inscription", component: InscriptionComponent},
     {path: "connexion", component: ConnexionComponent},
-    {path: "", redirectTo: "accueil", pathMatch: "full" },
     {path: "", component :Page404Component},
 ];
