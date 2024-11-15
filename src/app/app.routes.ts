16ea7d85d9ef615c2b300445e7124defca2446b6
PC_ALEXIS\alexis <alexis.bessac@gmail.com>
2024-11-15 16:47:29 +0100

    Mise en place des premières routes de l'application

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,11 @@
 import { Routes } from '@angular/router';
+import { ConnexionComponent } from './connexion/connexion.component';
+import { InscriptionComponent } from './inscription/inscription.component';
+import { Page404Component } from './page404/page404.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {path: "inscription", component: InscriptionComponent},
+    {path: "connexion", component: ConnexionComponent},
+    {path: "", redirectTo: "accueil", pathMatch: "full" },
+    {path: "", component :Page404Component},
+];
