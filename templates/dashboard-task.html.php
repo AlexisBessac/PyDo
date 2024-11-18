f53adb0a9f0e296970125b75b9becef050b472d2
PC_ALEXIS\alexis <alexis.bessac@gmail.com>
2024-11-18 16:13:02 +0100

    CSS du tableau de bord des tâches

--- templates/dashboard-task.html.php
+++ templates/dashboard-task.html.php
@@ -1,6 +1,6 @@
 <section class="container py-5">
     <h1 class="text-center">Ma liste de tâches</h1>
-    <div>
+    <div class="button-container">
         <a href="/?page=index"><button class="btn btn-primary" title="Se déconnecter">Se Déconnecter</button></a>
     </div>
 </section>
