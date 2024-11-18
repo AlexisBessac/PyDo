4369b44f1f13939d22d58a47dd7b523aafc2270f
PC_ALEXIS\alexis <alexis.bessac@gmail.com>
2024-11-18 16:16:19 +0100

    script de la déconnexion

--- src/pages/log-out.php
+++ src/pages/log-out.php
@@ -3,7 +3,8 @@
 session_start();
 
 // Si l'utilisateur est connecté et souhaite se déconnecter
-if (isset($_GET['logout'])) {
+if (isset($_GET['logout']))
+{
     // Détruire la session
     session_unset();
     session_destroy();
