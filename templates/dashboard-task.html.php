7854ed97c3656fa58bf0a11ab645b8da13b5ece5
PC_ALEXIS\alexis <alexis.bessac@gmail.com>
2024-11-18 16:10:54 +0100

    Mise en place de la déconnexion

--- templates/dashboard-task.html.php
+++ templates/dashboard-task.html.php
@@ -1,3 +1,6 @@
 <section class="container py-5">
     <h1 class="text-center">Ma liste de tâches</h1>
+    <div>
+        <a href="/?page=index"><button class="btn btn-primary" title="Se déconnecter">Se Déconnecter</button></a>
+    </div>
 </section>
