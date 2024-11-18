c48daf61511e3d19f1b32cd3dee10b803ff7f419
PC_ALEXIS\alexis <alexis.bessac@gmail.com>
2024-11-18 16:15:23 +0100

    Changement du nom du bouton de déconnexion pour l'utilisateur

--- templates/dashboard-task.html.php
+++ templates/dashboard-task.html.php
@@ -1,6 +1,6 @@
 <section class="container py-5">
     <h1 class="text-center">Ma liste de tâches</h1>
     <div class="button-container">
-        <a href="/?page=index"><button class="btn btn-primary" title="Se déconnecter">Se Déconnecter</button></a>
+        <a href="/?page=index"><button class="btn btn-primary" title="Déconnexion">Déconnexion</button></a>
     </div>
 </section>
