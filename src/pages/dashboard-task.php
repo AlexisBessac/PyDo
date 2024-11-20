2a4046c1f00be5791b997659f546c3248d5fc3c3
PC_ALEXIS\alexis <alexis.bessac@gmail.com>
2024-11-20 22:16:24 +0100

    Mise en place de la séparation des tâches visibles par chaque utilisateur

--- src/pages/dashboard-task.php
+++ src/pages/dashboard-task.php
@@ -1,9 +1,16 @@
 <?php
 
+session_start();
+
 $title = "Liste des tâches de l'utilisateur";
 $description = "Sur cette page l'utilisateur trouve la liste de ces tâches qu'il doit faire, en créer, les modifier et les supprimer";
 
 require '../src/data/db-connect.php';
 
-$query = $dbh->query("SELECT * FROM task");
-$task = $query->FETCHALL();
+// Remplacez cette ligne par la manière dont vous obtenez l'ID de l'utilisateur
+$id_utilisateur = $_SESSION['id_utilisateur'];
+
+$query = $dbh->prepare("SELECT * FROM task WHERE id_utilisateur = :id_utilisateur");
+$query->execute([':id_utilisateur' => $id_utilisateur]);
+
+$task = $query->fetchAll(); 
